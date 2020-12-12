c7bc2074fb42e9750f5d1704c794eab50712fd2d
Kyo <KyoZM@hitler.rocks>
2020-12-12 05:57:24 -0300

    remove custom font

--- src/main/java/com/kyozm/nanoutils/utils/FontDrawer.java
+++ src/main/java/com/kyozm/nanoutils/utils/FontDrawer.java
@@ -1,29 +1,22 @@
 package com.kyozm.nanoutils.utils;
 
-import external.font.CFontRenderer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 
 import java.awt.*;
 
 public class FontDrawer {
-    public static CFontRenderer smooth = new CFontRenderer(new Font("Tahoma", 0, 14), true, true);
     public static FontRenderer mcFont = Minecraft.getMinecraft().fontRenderer;
-    public static boolean useSmooth = true;
 
     public static void drawString(String string, int x, int y, Color color) {
-        if (useSmooth) {
-            smooth.drawString(string, x, y, color.getRGB());
-        } else {
             mcFont.drawString(string, x, y, color.getRGB());
-        }
     }
 
     public static int getStringWidth(String string) {
-        return useSmooth ? smooth.getStringWidth(string) : mcFont.getStringWidth(string);
+        return mcFont.getStringWidth(string);
     }
 
     public static int getFontHeight() {
-        return useSmooth ? smooth.getStringHeight("A"): mcFont.FONT_HEIGHT;
+        return mcFont.FONT_HEIGHT;
     }
 }
